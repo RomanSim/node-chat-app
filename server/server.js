238d2c0afe7bcd9b475fdae897d7a725ff8e7742
Roman Sima <romansima8@gmail.com>
2019-03-07 04:16:00 +0200

    Add new message and create message event

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Roman',
+        text: 'Hey there',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('Create message', newMessage);
+    });
+
     socket.on('disconnect', () => {
        console.log('User was Disconnected');
     });
